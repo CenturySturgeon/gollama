e98439052d1bcda089135b30611db8c7a69b6c7c
CenturySturgeon <gfsolucionesintegradas@gmail.com>
2023-08-10 19:17:31 -0600

    Added functionality to make queries to a LLM

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os/exec"
+	"strings"
+)
 
 type LLM struct {
 	model          string   // Path to the model.bin
@@ -27,10 +32,6 @@ func (llm *LLM) getLLMProps() {
 	fmt.Println("List of generation-stopping strings: ", llm.stop)
 }
 
-func buildCommand(llm *LLM) string {
-	return fmt.Sprintf("./llama.cpp/main -m %s --color   --ctx_size %d   -n -1   -ins -b 128   --top_k %d   --temp %.1f   --repeat_penalty %.1f   --n-gpu-layers %d   -t 8", llm.model, llm.ctx_size, llm.top_k, llm.temp, llm.repeat_penalty, llm.ngl)
-}
-
 func (llm *LLM) llmDefaults() {
 	if llm.model == "" {
 		llm.model = "./llama.cpp/models/ggml-vocab.bin"
@@ -55,13 +56,101 @@ func (llm *LLM) llmDefaults() {
 	}
 }
 
-func (llm *LLM) promptModel(prompt string) {
+func (llm *LLM) promptModel(prompts []string) {
 	llm.llmDefaults()
-	llm.getLLMProps()
-	fmt.Println(buildCommand(llm))
+	cmd, stdin, stdout, err := createPipes(buildCommand(llm))
+
+	if err != nil {
+		fmt.Println("Error creating pipes", err)
+		return
+	}
+
+	// Read and collect outputs
+	outputs := []string{}
+	buf := make([]byte, 1024)
+
+	for _, input := range prompts {
+
+		// Input must contain an EOL for the LLM to correctly interpret the propmt's end
+		if !strings.Contains(input, "\n") {
+			input += "\n"
+		}
+		// Sending input
+		io.WriteString(stdin, input)
+
+		output := ""
+		for {
+			n, err := stdout.Read(buf)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Println("Error reading token:", err)
+				}
+				break
+			}
+
+			token := string(buf[:n])
+			output = output + token
+
+			if strings.Count(output, ">") >= 2 {
+				break
+			}
+		}
+
+		fmt.Println("Completed reading output.")
+		outputs = append(outputs, strings.ReplaceAll(strings.ReplaceAll(output, "\n", ""), ">", ""))
+	}
+
+	// Print collected outputs
+	fmt.Println("Outputs: ", outputs)
+
+	// Close the communication with the LLM
+	closePipes(cmd, stdin, stdout)
+}
+
+func buildCommand(llm *LLM) string {
+	return fmt.Sprintf("./llama.cpp/main -m %s --color   --ctx_size %d   -n -1   -ins -b 128   --top_k %d   --temp %.1f   --repeat_penalty %.1f   --n-gpu-layers %d   -t 8", llm.model, llm.ctx_size, llm.top_k, llm.temp, llm.repeat_penalty, llm.ngl)
+}
+
+func createPipes(command string) (*exec.Cmd, io.WriteCloser, io.ReadCloser, error) {
+	cmd := exec.Command(command)
+
+	// Create pipes for stdin and stdout
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		fmt.Println("Error creating stdin pipe:", err)
+		return nil, nil, nil, err
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error creating stdout pipe:", err)
+		return nil, nil, nil, err
+	}
+
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting command:", err)
+		return nil, nil, nil, err
+	}
+
+	return cmd, stdin, stdout, nil
+}
+
+func closePipes(cmd *exec.Cmd, stdin io.WriteCloser, stdout io.ReadCloser) {
+
+	fmt.Println("Closing stdin")
+	// Close stdin explicitly
+	myerr := stdin.Close()
+
+	if myerr != nil {
+		fmt.Println("Error when closing the command :", myerr)
+	}
+
+	// Kill the process
+	cmd.Process.Kill()
 }
 
 func main() {
-	llm := LLM{model: "~/Ai/models/llama-2-13b-chat.ggmlv3.q4_0.bin", ctx_size: 1024, top_k: 1, temp: 0.5, repeat_penalty: 1.8, ngl: 30}
-	llm.promptModel("Hi")
+	llm := LLM{model: "~/Ai/models/llama-2-13b-chat.ggmlv3.q4_0.bin", ngl: 30}
+	llm.promptModel([]string{"Hi, how are you ?"})
 }
